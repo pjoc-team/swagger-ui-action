2d19bc7b9ef0ebfd7f8a2c07e473b47cfa285c28
blademainer <blademainer@gmail.com>
2020-10-29 15:20:57 +0800

    fix build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env sh
 
-mkdir -p /apis/
-find "${dir}" -name "${pattern}" -type f -exec cp "{}" /apis/  \;
+mkdir -p /usr/share/nginx/html/apis/
+mkdir -p ./swagger-ui/
+find "${dir}" -name "${pattern}" -type f -exec cp "{}" /usr/share/nginx/html/apis/  \;
 
-find /apis/
+find /usr/share/nginx/html/apis/
 
+SWAGGER_CONFIG=$(
+  cd /usr/share/nginx/html/apis/
+  find . -type f -name "${pattern}" | sed 's/.//' | sort | while read line; do
+    echo "{\"name\":\"${line:1}\",\"url\":\"/apis${line}\"}"
+  done | tr '\n' ',' | sed 's/.$//' | sed 's/$/]}/' | sed 's/^/{"urls":[/'
+)
+
+echo "$SWAGGER_CONFIG" > /usr/share/nginx/html/apis/swagger-config.json
+
+cp -r /usr/share/nginx/html/* ./swagger-ui/
 #git config --local user.email "blademainer@gmail.com"
 #git config --local user.name "blademainer"
 #[ -d "$PR_NUMBER" ] || mkdir "$PR_NUMBER"
